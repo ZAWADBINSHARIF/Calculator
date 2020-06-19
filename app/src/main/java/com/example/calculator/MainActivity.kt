bf388eb9c14e4b81572e85a93cd9460ac4a288d4
ZAWAD SHARIF <ZAWADSHARIF@github.com>
2020-06-19 19:29:55 +0600

    Design new layout and respond number digit , backSpace , AllClear

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onDigit(view: View) {
+        calculationView.append((view as Button).text)
+    }
+
+    fun onBackSpace(view: View) {
+        val calculation = calculationView.text.toString()
+
+        when {
+            calculation.isNotEmpty() -> {
+                calculationView.text = calculation.substring(0, calculation.length - 1)
+            }
+        }
+    }
+
+    fun onAllClear(view: View) {
+        calculationView.text = ""
+        resultView.text = ""
+    }
+
+    fun onDot(view: View) {
+
+    }
+
+    fun onBracket(view: View) {
+        calculationView.append((view as Button).text)
     }
 }
