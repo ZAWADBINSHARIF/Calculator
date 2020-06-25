5e60628036eb5de5388e6111f4d92bb9c47e8ec3
ZAWAD SHARIF <ZAWADSHARIF@github.com>
2020-06-25 12:49:08 +0600

    New layout

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,44 +1,50 @@
 package com.example.calculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
 import android.view.View
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        calculationView.movementMethod = ScrollingMovementMethod()
     }
 
     fun onDigit(view: View) {
-        calculationView.append((view as Button).text)
+
+    }
+
+    fun onOperator(view: View) {
+
     }
 
     fun onBackSpace(view: View) {
-        val calculation = calculationView.text.toString()
 
-        when {
-            calculation.isNotEmpty() -> {
-                calculationView.text = calculation.substring(0, calculation.length - 1)
-            }
-        }
     }
 
     fun onAllClear(view: View) {
-        calculationView.text = ""
-        resultView.text = ""
+
     }
 
     fun onDot(view: View) {
 
     }
 
-    fun onBracket(view: View) {
-        calculationView.append((view as Button).text)
+    fun onSin(view: View) {
+
     }
+
+    fun onCos(view: View) {
+
+    }
+
+    fun onEqual(view: View) {
+
+    }
+
 }
