aa149f068f964176ff3f8d3acba2b2036a680194
ZAWAD SHARIF <ZAWADSHARIF@github.com>
2020-06-29 13:15:43 +0600

    create perfect op Symbol logic

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -8,38 +8,56 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private var num1: Double? = null
-    private var num2: Double? = 0.0
-    var showResult = ""
-    var sResult = true
+
+    private var showOP = true
+    private var firstMinus = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         calculationView.movementMethod = ScrollingMovementMethod()
-
-        sResult = false
+        firstMinus = false
     }
 
     fun onDigit(view: View) {
-
+        resultView.append((view as Button).text)
+        showOP = false
+        firstMinus = true
     }
 
     fun onOperator(view: View) {
 
+        val op = (view as Button).text.toString()
+
+        if (!showOP) {
+            calculationView.append("${(view as Button).text}\n")
+            showOP = true
+            }
+        if (op == "-" && !firstMinus && resultView.text.isEmpty()) {
+            resultView.append("${(view as Button).text}")
+            showOP = true
+            firstMinus = true
+        }
     }
 
     fun onBackSpace(view: View) {
         if (resultView.text.isNotEmpty()) {
             val backSpace = resultView.text.substring(0, resultView.text.length - 1)
             resultView.text = backSpace
+
+            val isMinus = resultView.text[0]
+            firstMinus = isMinus == '-'
+
+            showOP = isMinus == '-' && resultView.text.toString().length == 1
         }
     }
 
     fun onAllClear(view: View) {
         calculationView.text = ""
         resultView.text = ""
+        showOP = true
+        firstMinus = false
     }
 
     fun onDot(view: View) {
