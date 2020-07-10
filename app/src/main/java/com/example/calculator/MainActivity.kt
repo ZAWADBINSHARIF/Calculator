4eb0ca105a9395e93bcd2f14f1843278dcb916f8
ZAWAD SHARIF <ZAWADSHARIF@github.com>
2020-07-10 17:54:44 +0600

    Complete all Button Logics But we need to Fix some Bugs, you fix this Bugs. i am done and exit this project now Good Bye

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -7,6 +7,8 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.Exception
+import java.lang.Math.pow
+import kotlin.math.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,6 +26,8 @@ class MainActivity : AppCompatActivity() {
     private var oneResult = false
     private var oneEquationResult: Double? = null
     private var canFactorial = true
+    private var canPower = false
+    private var canTenPow = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,12 +39,16 @@ class MainActivity : AppCompatActivity() {
 
     fun onDigit(view: View) {
 
-        if (    !(resultView.text.endsWith('%') ||
-                resultView.text.endsWith("y") ||
-                resultView.text.startsWith("I") ||
-                resultView.text.endsWith("N") ||
-                resultView.text.startsWith("N")||
-                    resultView.text.endsWith('!'))) {
+        if(resultView.text.length > 10)
+            resultView.textSize = 25f
+        else
+            resultView.textSize = 55f
+
+        if (
+            !(resultView.text.contains("NaN")) &&
+            !(resultView.text.contains("Infinity") &&
+                    !resultView.text.contains('π'))
+        ) {
 
         if (showResultCalculation == true){
             resultView.text = ""
@@ -75,6 +83,8 @@ class MainActivity : AppCompatActivity() {
                 canPercentage = false
                 oneResult = false
                 oneEquationResult = null
+                canPower = false
+                canTenPow = false
 
                 calculationView.append("\n__________________\nNew Calculation ${++newCalculationNum}\n__________________\n\n")
 
@@ -164,7 +174,8 @@ class MainActivity : AppCompatActivity() {
         showResultCalculation = null
         showEqual = false
         canPercentage = false
-
+        canPower = false
+        canTenPow = false
         oneResult = false
         oneEquationResult = null
     }
@@ -185,16 +196,36 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onPercentage(view: View) {
+        val showFactorial = resultView.text.contains('!')
         val showPercentage = resultView.text.contains('%')
-        if (!showPercentage && canPercentage) {
+        val showSin = resultView.text.contains("SIN")
+        val showCos = resultView.text.contains("COS")
+        val showTan = resultView.text.contains("TAN")
+        val showLog = resultView.text.contains("LOG")
+        val showPow = resultView.text.contains('^')
+        val showRoot = resultView.text.contains('√')
+
+        if (!showPercentage && canPercentage &&
+                !(showPercentage && showCos && showSin && showTan && showLog && showPow && showRoot)) {
             resultView.append((view as Button).text)
             canPercentage = true
         }
     }
 
     fun onFactorial(view: View) {
+        val showPercentage = resultView.text.contains('%')
         val showFactorial = resultView.text.contains('!')
-        if (!showFactorial && canFactorial) {
+        val showSin = resultView.text.contains("SIN")
+        val showCos = resultView.text.contains("COS")
+        val showTan = resultView.text.contains("TAN")
+        val showLog = resultView.text.contains("LOG")
+        val showPow = resultView.text.contains('^')
+        val showRoot = resultView.text.contains('√')
+
+        if (!showFactorial &&
+            canFactorial &&
+            resultView.text.isNotEmpty() &&
+                !(showPercentage && showCos && showSin && showTan && showLog && showPow && showRoot)) {
             resultView.append((view as Button).text)
             canFactorial = true
         }
@@ -211,11 +242,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onINV(view: View) {
+        val showFactorial = resultView.text.contains('!')
+        val showPercentage = resultView.text.contains('%')
+        val showSin = resultView.text.contains("SIN")
+        val showCos = resultView.text.contains("COS")
+        val showTan = resultView.text.contains("TAN")
+        val showLog = resultView.text.contains("LOG")
+        val showPow = resultView.text.contains('^')
+        val showRoot = resultView.text.contains('√')
+
         if (resultView.text.isNotEmpty() &&
             !(resultView.text.endsWith("y") ||
                     resultView.text.startsWith("I") ||
                     resultView.text.endsWith("N") ||
-                    resultView.text.startsWith("N")))
+                    resultView.text.startsWith("N")) &&
+            (!showFactorial &&
+                    canFactorial &&
+                    resultView.text.isNotEmpty() &&
+                    !(showPercentage && showCos && showSin && showTan && showLog && showPow && showRoot && showPercentage)))
         {
             val invNum = "${resultView.text.toString().toDouble() * -1}"
 
@@ -230,85 +274,65 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun onEqual(view: View) {
+    fun onSin(v : View) {
 
-        if (showResult != null) {
-        val showLastChar = calculationView.text.toString().length - 2
-        val removeLastSymbol = calculationView.text.toString()[showLastChar]
-        val removeDot = showResult!!.toString().endsWith(".0")
-        val newShowResult = showResult!!.toString().substring(0, showResult.toString().length - 2)
-
-        if(
-            (
-                    removeLastSymbol == '+' ||
-                    removeLastSymbol == '-' ||
-                    removeLastSymbol == '*' ||
-                    removeLastSymbol == '/'
-                    ) &&
-                !showEqual &&
-                showResultCalculation == true)
+        if(resultView.text.isEmpty())
         {
-            calculationView.text = calculationView.text.substring(0, showLastChar)
-            if (removeDot) {
-                calculationView.append("__________________\n$newShowResult\n")
-                resultView.text = newShowResult
-            }else {
-                calculationView.append("__________________\n$showResult\n")
-                resultView.text = showResult.toString()
-            }
-            showEqual = true
+            resultView.append((v as Button).text)
         }
+    }
+
+    fun onCos(v : View) {
+
+        if(resultView.text.isEmpty())
+        {
+            resultView.append((v as Button).text)
         }
+    }
 
-        if (
-            (showResultCalculation == false || showResultCalculation == null) &&
-            num2 == null &&
-            num1 != null &&
-            !showEqual &&
-            resultView.text.isNotEmpty()
-        ) {
-            storeResult(resultView.text.toString())
-            calculationView.append("${resultView.text}\n")
-            num2 = sResult
+    fun onTan(v : View) {
 
-            calculation(storeOP!!)
-
-            num1 = null
-            num2 = 0.0
-
-            val removeDot = showResult!!.toString().endsWith(".0")
-            val newShowResult = showResult!!.toString().substring(0, showResult.toString().length - 2)
-
-            if (removeDot) {
-                calculationView.append("__________________\n$newShowResult\n")
-                resultView.text = newShowResult
-            }else {
-                calculationView.append("__________________\n$showResult\n")
-                resultView.text = showResult.toString()
-            }
-
-            showEqual = true
+        if(resultView.text.isEmpty())
+        {
+            resultView.append((v as Button).text)
         }
+    }
 
-        if ((resultView.text.contains('%')) ||
-            (resultView.text.contains('!'))) {
-            oneEquation(resultView.text.toString())
-
-            val removeDot = oneEquationResult!!.toString().endsWith(".0")
-            val newOneEquationResult = oneEquationResult!!.toString().substring(0, oneEquationResult.toString().length - 2)
-
-            if(removeDot) {
-                resultView.text = newOneEquationResult
-                calculationView.append("\n$newOneEquationResult\n\n")
-            }else{
-                resultView.text = "$oneEquationResult"
-                calculationView.append("\n$oneEquationResult\n\n")
-            }
-            showEqual = true
-            showResultCalculation = true
+    fun onLog(v : View) {
+        if(resultView.text.isEmpty())
+        {
+            resultView.append((v as Button).text)
         }
-        }
+    }
 
+    fun onRoot(v : View) {
+
+        if(resultView.text.isEmpty())
+        {
+            resultView.append((v as Button).text)
+        }
+    }
+
+    fun onPI(view: View) {
+        if(resultView.text.isEmpty())
+            resultView.append((view as Button).text)
+        if(showResultCalculation == true)
+            resultView.text = ""
+    }
+
+    fun onPower (view: View) {
+        if(resultView.text.isNotEmpty() && !canPower && !canTenPow) {
+            resultView.append((view as Button).text)
+            canPower = true
+        }
+    }
+
+    fun onTenPow (view: View) {
+        if(resultView.text.isNotEmpty() && !canPower && !canTenPow) {
+            resultView.append((view as Button).text)
+            canTenPow = true
+        }
+    }
 
     private fun storeResult(sNumber: String) : Double {
 
@@ -325,6 +349,63 @@ class MainActivity : AppCompatActivity() {
                 factorial(factorialNumber.toDouble())
                 sResult as Double
             }
+
+            resultView.text.startsWith("SIN") -> {
+                val perfactNum = sNumber.substring(3, sNumber.length)
+                sResult = sin(perfactNum.toDouble() * 2 * (PI/360))
+                sResult as Double
+            }
+
+            resultView.text.startsWith("COS") -> {
+                val perfactNum = sNumber.substring(3, sNumber.length)
+                sResult = cos(perfactNum.toDouble() * 2 * (PI/360))
+                sResult as Double
+            }
+
+            resultView.text.startsWith("TAN") -> {
+                val perfactNum = sNumber.substring(3, sNumber.length)
+                sResult = tan(perfactNum.toDouble()* 2 * (PI/360))
+                sResult as Double
+            }
+
+            resultView.text.startsWith("LOG") -> {
+                val perfactNum = sNumber.substring(3, sNumber.length)
+                sResult = log10(perfactNum.toDouble())
+                sResult as Double
+            }
+
+            resultView.text.startsWith("√") -> {
+                val perfactNum = sNumber.substring(1, sNumber.length)
+                sResult = sqrt(perfactNum.toDouble())
+                sResult as Double
+            }
+
+            resultView.text.startsWith("π") -> {
+                sResult = PI
+                sResult as Double
+            }
+
+            resultView.text.contains("^") && !canTenPow && canPower -> {
+                val power = resultView.text.split('^')
+                val num = power[0]
+                val powerNum = power[1]
+
+                sResult = num.toDouble().pow(powerNum.toDouble())
+                canPower = false
+                sResult as Double
+            }
+
+            resultView.text.contains("*10^") && canTenPow && !canPower -> {
+                val tenPow = resultView.text.split("*10^")
+                val num = tenPow[0]
+                val powerNum = tenPow[1]
+
+                sResult = (num.toDouble() * 10).pow(powerNum.toDouble())
+                canTenPow = false
+                sResult as Double
+            }
+
+
 
             else -> {
                 sResult = sNumber.toDouble()
@@ -494,10 +575,154 @@ class MainActivity : AppCompatActivity() {
             val fact = factorialNumber.toDouble()
             factorial(fact)
         }
+
+            resultView.text.startsWith("SIN") -> {
+                val perfactNum = Values.substring(3, Values.length)
+                oneEquationResult = sin(perfactNum.toDouble() * 2 * (PI/360))
+                oneEquationResult as Double
+            }
+
+            resultView.text.startsWith("COS") -> {
+                val perfactNum = Values.substring(3, Values.length)
+                oneEquationResult = cos(perfactNum.toDouble() * 2 * (PI/360))
+                oneEquationResult as Double
+            }
+
+            resultView.text.startsWith("TAN") -> {
+                val perfactNum = Values.substring(3, Values.length)
+                oneEquationResult = tan(perfactNum.toDouble()* 2 * (PI/360))
+                oneEquationResult as Double
+            }
+
+            resultView.text.startsWith("LOG") -> {
+                val perfactNum = Values.substring(3, Values.length)
+                oneEquationResult = log10(perfactNum.toDouble())
+                oneEquationResult as Double
+            }
+
+            resultView.text.startsWith("√") -> {
+                val perfactNum = Values.substring(1, Values.length)
+                oneEquationResult = sqrt(perfactNum.toDouble())
+                oneEquationResult as Double
+            }
+
+            resultView.text.startsWith("π") -> {
+                oneEquationResult = PI
+                oneEquationResult as Double
+            }
+
+            resultView.text.contains("^") && !canTenPow && canPower -> {
+                val power = resultView.text.split('^')
+                val num = power[0]
+                val powerNum = power[1]
+
+                oneEquationResult = num.toDouble().pow(powerNum.toDouble())
+                canPower = false
+                oneEquationResult as Double
+            }
+
+            resultView.text.contains("*10^") && canTenPow && !canPower -> {
+                val tenPow = resultView.text.split("*10^")
+                val num = tenPow[0]
+                val powerNum = tenPow[1]
+
+                oneEquationResult = (num.toDouble() * 10).pow(powerNum.toDouble())
+                canTenPow = false
+                oneEquationResult as Double
+            }
+
             else -> {
                 oneEquationResult = null
                 return oneEquationResult as Double
             }
+        }
+    }
+
+    fun onEqual(view: View) {
+
+        if (showResult != null) {
+            val showLastChar = calculationView.text.toString().length - 2
+            val removeLastSymbol = calculationView.text.toString()[showLastChar]
+            val removeDot = showResult!!.toString().endsWith(".0")
+            val newShowResult = showResult!!.toString().substring(0, showResult.toString().length - 2)
+
+            if(
+                (
+                        removeLastSymbol == '+' ||
+                                removeLastSymbol == '-' ||
+                                removeLastSymbol == '*' ||
+                                removeLastSymbol == '/'
+                        ) &&
+                !showEqual &&
+                showResultCalculation == true)
+            {
+                calculationView.text = calculationView.text.substring(0, showLastChar)
+                if (removeDot) {
+                    calculationView.append("__________________\n$newShowResult\n")
+                    resultView.text = newShowResult
+                }else {
+                    calculationView.append("__________________\n$showResult\n")
+                    resultView.text = showResult.toString()
+                }
+                showEqual = true
+            }
+        }
+
+        if (
+            (showResultCalculation == false || showResultCalculation == null) &&
+            num2 == null &&
+            num1 != null &&
+            !showEqual &&
+            resultView.text.isNotEmpty()
+        ) {
+            storeResult(resultView.text.toString())
+            calculationView.append("${resultView.text}\n")
+            num2 = sResult
+
+            calculation(storeOP!!)
+
+            num1 = null
+            num2 = 0.0
+
+            val removeDot = showResult!!.toString().endsWith(".0")
+            val newShowResult = showResult!!.toString().substring(0, showResult.toString().length - 2)
+
+            if (removeDot) {
+                calculationView.append("__________________\n$newShowResult\n")
+                resultView.text = newShowResult
+            }else {
+                calculationView.append("__________________\n$showResult\n")
+                resultView.text = showResult.toString()
+            }
+
+            showEqual = true
+        }
+
+        if ((resultView.text.contains('%')) ||
+            (resultView.text.contains('!')) ||
+            resultView.text.contains("SIN")||
+            resultView.text.contains("COS")||
+            resultView.text.contains("TAN")||
+            resultView.text.contains("LOG")||
+            resultView.text.contains('^')||
+            resultView.text.contains('√')||
+                resultView.text.contains('π')||
+                resultView.text.contains("*10^"))
+        {
+            oneEquation(resultView.text.toString())
+
+            val removeDot = oneEquationResult!!.toString().endsWith(".0")
+            val newOneEquationResult = oneEquationResult!!.toString().substring(0, oneEquationResult.toString().length - 2)
+
+            if(removeDot) {
+                resultView.text = newOneEquationResult
+                calculationView.append("\n$newOneEquationResult\n\n")
+            }else{
+                resultView.text = "$oneEquationResult"
+                calculationView.append("\n$oneEquationResult\n\n")
+            }
+            showEqual = true
+            showResultCalculation = true
         }
     }
 }
